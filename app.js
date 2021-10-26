ae3b7b35d3494d7f2172527f92aadcfc81292a01
paulok <paulok@comprovei.com>
2021-10-26 01:46:03 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+import { searchbar } from './modules/epic-searchbar/index.js';
+document.addEventListener("DOMContentLoaded", function(){
+    searchbar();
+});
---
+++ modules/epic-searchbar/index.js
@@ -0,0 +1,4 @@
+'use strict';
+export function searchbar(){
+    console.log('searchbar');
+}
